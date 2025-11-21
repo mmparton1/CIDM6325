585a76fdfb31dfe18a3b49978e50c92fec6d34ab
Melodi <162312713+mmparton1@users.noreply.github.com>
2025-11-21 07:29:17 -0600

    Organized repo: move blog project into blog_project/, organize smart_scheduler templates, remove old root files

--- smart_scheduler/smart_scheduler/settings.py
+++ smart_scheduler/smart_scheduler/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'smart_scheduler.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
